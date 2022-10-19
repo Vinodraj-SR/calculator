e19a314a34346c5431d5affb995c32fbebd0eeb7
VINODRAJ-SR <vinodrajsr2001@gmail.com>
2022-10-19 16:50:14 +0530

    new1

--- calc.java
+++ calc.java
@@ -2,5 +2,6 @@ public class calc{
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("this is new code");
+        int a=10,b=11;
     }
 }
